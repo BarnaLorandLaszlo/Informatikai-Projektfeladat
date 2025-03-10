9f8807a5e5566164bf9d2fa577dcc4ed43ec9f24
Barna Loránd László <traktor.mtz1970@gmail.com>
2025-03-10 13:54:37 +0100

    database

---
+++ android app/app/src/main/java/com/example/filmajnlalkalmazs/database/UserDatabaseHelper.java
@@ -0,0 +1,85 @@
+package com.example.filmajnl.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class UserDatabaseHelper extends SQLiteOpenHelper {
+
+    // Adatbázis konstansok
+    private static final String DATABASE_NAME = "user_database.db";
+    private static final int DATABASE_VERSION = 1;
+
+    // Felhasználók tábla
+    private static final String TABLE_USERS = "users";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_USERNAME = "username";
+    private static final String COLUMN_FIRST_NAME = "first_name";
+    private static final String COLUMN_LAST_NAME = "last_name";
+    private static final String COLUMN_PASSWORD = "password";
+    private static final String COLUMN_EMAIL = "email";
+    private static final String COLUMN_REGISTRATION_DATE = "registration_date";
+    private static final String COLUMN_BIRTH_DATE = "birth_date";
+
+    // SQL tábla létrehozása
+    private static final String CREATE_TABLE_USERS =
+            "CREATE TABLE " + TABLE_USERS + " (" +
+                    COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    COLUMN_USERNAME + " TEXT NOT NULL UNIQUE, " +
+                    COLUMN_FIRST_NAME + " TEXT NOT NULL, " +
+                    COLUMN_LAST_NAME + " TEXT NOT NULL, " +
+                    COLUMN_PASSWORD + " TEXT NOT NULL, " +
+                    COLUMN_EMAIL + " TEXT NOT NULL UNIQUE, " +
+                    COLUMN_REGISTRATION_DATE + " TEXT NOT NULL, " +
+                    COLUMN_BIRTH_DATE + " TEXT NOT NULL" +
+                    ");";
+
+    public UserDatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE_USERS);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_USERS);
+        onCreate(db);
+    }
+
+    // Felhasználó hozzáadása
+    public long addUser(String username, String firstName, String lastName, String password, String email, String registrationDate, String birthDate) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_USERNAME, username);
+        values.put(COLUMN_FIRST_NAME, firstName);
+        values.put(COLUMN_LAST_NAME, lastName);
+        values.put(COLUMN_PASSWORD, password);
+        values.put(COLUMN_EMAIL, email);
+        values.put(COLUMN_REGISTRATION_DATE, registrationDate);
+        values.put(COLUMN_BIRTH_DATE, birthDate);
+
+        long result = db.insert(TABLE_USERS, null, values);
+        db.close();
+        return result;
+    }
+
+    // Felhasználó keresése név alapján
+    public Cursor getUserByUsername(String username) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        return db.rawQuery("SELECT * FROM " + TABLE_USERS + " WHERE " + COLUMN_USERNAME + "=?", new String[]{username});
+    }
+
+    // Összes felhasználó lekérdezése
+    public Cursor getAllUsers() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        return db.rawQuery("SELECT * FROM " + TABLE_USERS, null);
+    }
+}
+
+
+
